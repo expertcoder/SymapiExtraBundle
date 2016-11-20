289e479a07f5359e635679749d1d4d000654fdb5
Sam Anthony <sam_anthony99@hotmail.com>
2016-11-20 20:00:27 +0200

    Corrections

--- ExpertCoderSymapiExtraBundle.php
+++ ExpertCoderSymapiExtraBundle.php
@@ -1,9 +1,18 @@
 <?php
 
-namespace ExpertCoder\SymapiSecurityBundle;
+namespace ExpertCoder\SymapiExtraBundle;
 
+use ExpertCoder\SymapiExtraBundle\DependencyInjection\Complier\OverrideServiceCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ExpertCoderSymapiExtraBundle extends Bundle
 {
+	public function build(ContainerBuilder $container)
+	{
+		parent::build($container);
+
+		$container->addCompilerPass(new OverrideServiceCompilerPass());
+	}
+
 }
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # SymapiExtraBundle
+
+##Installation
+
+Install with composer
+
+````
+composer require expertcoder/symapi-extra-bundle
+````
+
+Include in AppKernal.php
+
+````
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = [
+            ....
+            new ExpertCoder\SymapiExtraBundle\ExpertCoderSymapiExtraBundle(),
+            ....
+        ];
+
+````
